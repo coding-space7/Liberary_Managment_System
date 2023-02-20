5d7c77799e82b10a3b345112a8fcbb497b6ae1c2
coding-space7 <sawbharat2001@gmail.com>
2023-02-20 18:09:42 +0530

    commit

--- src/main/java/com/example/LiberaryManagmentSystem/Repository/TransactionRepository.java
+++ src/main/java/com/example/LiberaryManagmentSystem/Repository/TransactionRepository.java
@@ -1,9 +1,10 @@
 package com.example.LiberaryManagmentSystem.Repository;
 
+import com.example.LiberaryManagmentSystem.Models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<TransactionRepository,Integer> {
+public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
 
 }
